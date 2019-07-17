ac0065e7a12fe83fa28310f2d72035691f6dd4d1
Tate Shepard <tateshepard@yahoo.com>
2019-07-17 04:07:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,4 @@
-def introduction (name)
-  puts "Hi, my name is #{name}."
+def introduction(name)
+puts "Hi my name is #(name)"
 end
-
-def introduction_with_language (name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+  
